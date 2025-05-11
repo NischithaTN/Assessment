c38b4460809fad5f8ea8f09f67ec7e5c2d41cd15
NischithaTN <nischitha.n@s.amity.edu>
2025-05-11 22:33:11 +0530

    part2

---
+++ experiment2.c
@@ -0,0 +1,18 @@
+switch (choice) {
+        case 1:
+            printf("You chose CSE.\n");
+            break;
+       case 2:
+            printf("you chose AIML.\n");
+            break;
+       case 3:
+           printf("you chose ECE.\n");
+          break;
+       case 4:
+           printf("you chose Mechanics.\n")
+           break;
+       default:
+           printf("invalid choice\n")
+   }
+    return 0;
+}
